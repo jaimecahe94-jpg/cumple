141565063dd87e37556e79412ae860f12c0e2ca6
jorgeArche <79043031+jorgeArche@users.noreply.github.com>
2026-02-10 11:29:11 +0100

    Fix closing tag for subtitle-rave div

--- src/App.tsx
+++ src/App.tsx
@@ -261,7 +261,7 @@ function App() {
 
       <header>
         <h1 className="main-title">IMPERDIBLE</h1>
-        <div className="subtitle-rave">30 Años de caos planificado/div>
+        <div className="subtitle-rave">30 Años de caos planificado</div>
         <div className="age-badge">GENERACIÓN '96</div>
         <div className="date-rave">
           21 FEB 11:00 ▶ 22 FEB 8:00<br />
